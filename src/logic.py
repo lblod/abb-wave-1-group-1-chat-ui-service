edacf8f10cedcdf5d9caae17cca40db55c57173a
matthiasdr <matthiasderoo28@gmail.com>
2024-09-24 11:32:15 +0200

    feat: llm client

---
+++ src/logic.py
@@ -0,0 +1,34 @@
+import os
+from ollama import Client
+
+
+class LLMClient:
+    
+    #http://hackathon-ai-1.s.redhost.be/11434
+    def __init__(self, api=os.environ.get('OLLAMAHOST', 'http://hackathon-ai-1.s.redhost.be/11434'), model="llama3.1"):
+        self.llm = Client(host=api)
+        self.model = model
+        
+    def preprocess_input(self, message, history):
+        messages = []
+        
+        for old_turn in history:
+            old_turn = [old_turn] if isinstance(old_turn, str) else old_turn
+            
+            for i, old_message in enumerate(old_turn):
+                messages.append({
+                    'role': 'user' if i%2 == 0 else 'assistant',
+                    'content': old_message if isinstance(old_message, str) else old_message[i%2]
+                })
+            
+        messages.append({
+            'role': 'user',
+            'content': message
+        })
+        
+        return messages
+        
+    def chat(self, messages:list[dict[str, str]]):
+        response = self.llm.chat(model=self.model, messages=messages)
+        print(response)
+        return response['message']['content']
--- src/main.py
+++ src/main.py
@@ -1,19 +1,21 @@
 import gradio as gr
 
+from logic import LLMClient
+
+llmClient = LLMClient()
+
 def yes_man(message, history):
-    if message.endswith("?"):
-        return "Yes"
-    else:
-        return "Ask me anything!"
+    preprocessed = llmClient.preprocess_input(message, history)
+    return llmClient.chat(preprocessed)
 
 gr.ChatInterface(
     yes_man,
     chatbot=gr.Chatbot(height=300),
-    textbox=gr.Textbox(placeholder="Ask me a yes or no question", container=False, scale=7),
-    title="Yes Man",
-    description="Ask Yes Man any question",
+    textbox=gr.Textbox(placeholder="Hi! how can I help you?", container=False, scale=7),
+    title="Heritage AI",
+    description="",
     theme="soft",
-    examples=["Hello", "Am I cool?", "Are tomatoes vegetables?"],
+    examples=[],
     cache_examples=True,
     retry_btn=None,
     undo_btn="Delete Previous",
